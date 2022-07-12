90481889d06fa59a49207d5b8b02cad2d921d628
wuxt <1752742392@qq.com>
2022-07-12 19:43:08 +0800

    首页热门品牌

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -13,6 +13,8 @@
   <HomeNew />
   <!-- 人气推荐 -->
   <HomeHot />
+  <!-- 热门品牌 -->
+  <HomeBrand />
 </template>
 
 <script setup>
@@ -20,6 +22,7 @@ import HomeCategory from './components/home-category'
 import HomeBanner from './components/home-banner.vue'
 import HomeNew from './components/home-new'
 import HomeHot from './components/home-hot'
+import HomeBrand from './components/home-brand.vue'
 </script>
 
 <style scoped lang="less">
