ba6d08336e7e39d945c62f14723f0430f7c253d4
wuxt <1752742392@qq.com>
2022-07-13 16:51:58 +0800

    排序组件

--- src/views/category/sub.vue
+++ src/views/category/sub.vue
@@ -5,6 +5,12 @@
       <SubBread />
       <!-- 筛选区 -->
       <SubFilter />
+      <!-- 结果区域 -->
+      <div class="goods-list">
+        <!-- 排序 -->
+        <SubSort />
+        <!-- 列表 -->
+      </div>
     </div>
   </div>
 </template>
@@ -12,4 +18,13 @@
 <script setup>
 import SubBread from './components/sub-bread'
 import SubFilter from './components/sub-filter'
+import SubSort from './components/sub-sort'
 </script>
+
+<style scoped lang="less">
+.goods-list {
+  background: #fff;
+  padding: 0 25px;
+  margin-top: 25px;
+}
+</style>
