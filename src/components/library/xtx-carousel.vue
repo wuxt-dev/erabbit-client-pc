85d35f25ddbf4ff33618d6dd57d2710258992f87
wuxt <1752742392@qq.com>
2022-07-14 21:56:36 +0800

    同类推荐

--- src/components/library/xtx-carousel.vue
+++ src/components/library/xtx-carousel.vue
@@ -7,9 +7,22 @@
         :key="i"
         :class="{ fade: index === i }"
       >
-        <RouterLink to="/">
+        <!-- 图片 -->
+        <RouterLink v-if="item.imgUrl" :to="item.hrefUrl">
           <img :src="item.imgUrl" alt="" />
         </RouterLink>
+        <!-- 商品列表 -->
+        <div v-else class="slider">
+          <RouterLink
+            v-for="goods in item"
+            :key="goods.id"
+            :to="`/product/${goods.id}`"
+          >
+            <img :src="goods.picture" alt="" />
+            <p class="name ellipsis">{{ goods.name }}</p>
+            <p class="price">&yen;{{ goods.price }}</p>
+          </RouterLink>
+        </div>
       </li>
     </ul>
     <a href="javascript:;" class="carousel-btn prev" @click="toggle(-1)"
@@ -179,6 +192,31 @@ export default {
   &:hover {
     .carousel-btn {
       opacity: 1;
+    }
+  }
+}
+// 轮播商品
+.slider {
+  display: flex;
+  justify-content: space-around;
+  padding: 0 40px;
+  > a {
+    width: 240px;
+    text-align: center;
+    img {
+      padding: 20px;
+      width: 230px !important;
+      height: 230px !important;
+    }
+    .name {
+      font-size: 16px;
+      color: #666;
+      padding: 0 40px;
+    }
+    .price {
+      font-size: 16px;
+      color: @priceColor;
+      margin-top: 15px;
     }
   }
 }
