cd5c008f141876368993b733da415790b03bb763
wuxt <1752742392@qq.com>
2022-07-14 10:33:31 +0800

    商品图片放大镜

--- src/views/goods/components/goods-image.vue
+++ src/views/goods/components/goods-image.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="goods-image">
-    <div class="middle">
+    <div
+      v-show="show"
+      class="large"
+      :style="[{ backgroundImage: `url(${images[currIndex]})` }, bgPosition]"
+    ></div>
+    <div class="middle" ref="target">
       <img :src="images[currIndex]" alt="" />
+      <div class="layer" :style="position"></div>
     </div>
     <ul class="small">
       <li
@@ -15,7 +21,8 @@
   </div>
 </template>
 <script setup>
-import { ref, defineProps } from 'vue'
+import { ref, defineProps, reactive, watch } from 'vue'
+import { useMouseInElement } from '@vueuse/core'
 defineProps({
   images: {
     type: Array,
@@ -23,6 +30,36 @@ defineProps({
   }
 })
 const currIndex = ref(0)
+
+const target = ref(null)
+const show = ref(false)
+// elementX 鼠标基于容器左上角X轴偏移
+// elementY 鼠标基于容器左上角Y轴偏移
+// isOutside 鼠标是否在模板容器外
+const { elementX, elementY, isOutside } = useMouseInElement(target)
+const position = reactive({ left: 0, top: 0 })
+const bgPosition = reactive({
+  backgroundPositionX: 0,
+  backgroundPositionY: 0
+})
+watch([elementX, elementY, isOutside], () => {
+  // 控制X轴方向的定位 0-200 之间
+  if (elementX.value < 100) position.left = 0
+  else if (elementX.value > 300) position.left = 200
+  else position.left = elementX.value - 100
+  // 控制Y轴方向的定位 0-200 之间
+  if (elementY.value < 100) position.top = 0
+  else if (elementY.value > 300) position.top = 200
+  else position.top = elementY.value - 100
+  // 设置大背景的定位
+  bgPosition.backgroundPositionX = -position.left * 2 + 'px'
+  bgPosition.backgroundPositionY = -position.top * 2 + 'px'
+  // 设置遮罩容器的定位
+  position.left = position.left + 'px'
+  position.top = position.top + 'px'
+  // 设置是否显示预览大图
+  show.value = !isOutside.value
+})
 </script>
 <style scoped lang="less">
 .goods-image {
@@ -30,10 +67,32 @@ const currIndex = ref(0)
   height: 400px;
   position: relative;
   display: flex;
+  z-index: 500;
+  .large {
+    position: absolute;
+    top: 0;
+    left: 412px;
+    width: 400px;
+    height: 400px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    background-repeat: no-repeat;
+    background-size: 800px 800px;
+    background-color: #f8f8f8;
+  }
   .middle {
     width: 400px;
     height: 400px;
     background: #f5f5f5;
+    position: relative;
+    cursor: move;
+    .layer {
+      width: 200px;
+      height: 200px;
+      background: rgba(0, 0, 0, 0.2);
+      left: 0;
+      top: 0;
+      position: absolute;
+    }
   }
   .small {
     width: 80px;
